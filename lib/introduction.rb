93f0b4c8becba1367288587adfa2c2b2a5f42e96
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-01 22:18:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,10 @@ end
 introduction_with_language(Luis,Python)
 
 def introduction_with_language_optional(name,language = 'Ruby')
-  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional(Luis)
 
 
 
